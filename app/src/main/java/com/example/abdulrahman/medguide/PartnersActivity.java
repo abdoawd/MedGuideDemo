9fa356c56eeb59d2862523173dc984f5ceb4e613
abdulrahman <awadabdo222@gmail.com>
2018-08-31 20:54:57 +0200

    Initial commit

---
+++ app/src/main/java/com/example/abdulrahman/medguide/PartnersActivity.java
@@ -0,0 +1,13 @@
+package com.example.abdulrahman.medguide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PartnersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_partner);
+    }
+}
